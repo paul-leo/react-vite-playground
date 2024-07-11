cbf992f769bfb7bdb068674dcaa1a6764463016b
roc <roc.liu@hotmail.com>
2024-07-11 17:20:57 +0800

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
 import TableView from './components/Demo';
-import Million from './components/Million';
 import './App.css';
 
 function App() {
     return (
         <>
-            {/* <TableView /> */}
-            <Million />
+            <TableView />
         </>
     );
 }
